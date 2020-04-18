e259abfdd8e48aec7684f2f4799230bb5adeda20
1977016033 <1977016033@qq.com>
2020-04-18 09:58:06 +0800

    优查小程序

---
+++ YYouC/miniprogram/pages/HomePage/HomePage.js
@@ -0,0 +1,72 @@
+// miniprogram/pages/HomePage/HomePage.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      HomePage:[{"id":"1","src":"/images/chers/idea.jpg","home_name":"Idea开发工具安装","home_introduce":"Idea工具具体安装步骤，附JDK、破解方法......."},
+      {"id":"2","src":"/images/chers/wx.jpg","home_name":"微信小程序开发工具","home_introduce":"微信小程序开发工具具体安装步骤，附注册，开发码......."},
+      {"id":"3","src":"/images/chers/Navicat.png","home_name":"Navicat数据库安装","home_introduce":"Navicat数据库具体安装步骤，附破解方法......."},
+      {"id":"4","src":"/images/chers/MySql.jpg","home_name":"MySql开源数据库安装","home_introduce":"MySql数据库具体安装步骤，附安装具体流程......."},
+      {"id":"4","src":"/images/chers/Pycharm.jpg","home_name":"Pycharm开发工具安装","home_introduce":"Pycharm具体安装步骤，附安装具体流程......."},
+      {"id":"4","src":"/images/chers/wx.jpg","home_name":"第一个项目的编写","home_introduce":"第一次接触程序世界的“我们”，你好Hello Wrold......."}
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
